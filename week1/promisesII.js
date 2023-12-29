fd09c73f70c8ece9d736acb970457a8d5f97db10
ayanahmed05 <ayan005.aa@gmail.com>
2023-12-29 21:46:11 +0530

    II

--- week1/promisesII.js
+++ week1/promisesII.js
@@ -12,7 +12,7 @@ var medicine = medicine1get(); // so medicine is an object
 console.log(medicine)
 //**Output  Promise {pending} */
 
-
+// ...medicine.catch.apply.apply.apply.apply.apply.
 console.log(medicine.then)
 
 // console.log(Object.keys(medicine)) .. // return keys;
