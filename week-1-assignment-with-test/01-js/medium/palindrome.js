9a8287c9db8bceb2bb63ad702919e053709f8661
ayanahmad11 <ayan2011032@akgec.ac.in>
2023-12-30 14:48:14 +0530

    solved

--- week-1-assignment-with-test/01-js/medium/palindrome.js
+++ week-1-assignment-with-test/01-js/medium/palindrome.js
@@ -5,9 +5,35 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
-
-function isPalindrome(str) {
+function isalnum(input) {
+  for (let i = 0; i < input.length; i++) {
+    const char = input.charAt(i);
+    if (!(char >= 'a' && char <= 'z') &&
+        !(char >= 'A' && char <= 'Z') &&
+        !(char >= '0' && char <= '9')) {
+      return false;
+    }
+  }
   return true;
 }
-
+function isPalindrome(str) {
+  let i=0,j=str.length-1;
+  while(i<=j){
+    if(!isalnum(str[i])){i++; continue;}
+    if(!isalnum(str[j])){j--;continue;}
+    if(str[i].toLowerCase()!=str[j].toLowerCase())
+      return false;
+    else {
+      i++;j--;
+    }  
+  }
+  return true;
+  
+}
+// console.log(isPalindrome("race a car"))
 module.exports = isPalindrome;
+
+// Note
+//The provided isPalindrome function doesn't explicitly handle spaces 
+// within the string. It treats spaces as non-alphanumeric characters due 
+// to the implementation of the isalnum helper function.
