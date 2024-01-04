6ff6251351a7168eddbefb1540db28539128dcaf
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-01-04 13:45:02 +0530

    done

--- week2/index.js
+++ week2/index.js
@@ -5,17 +5,36 @@ const port = 3000
 
 function handleFirstRequest(req,res){
     // res.send('Hello WOrld!')
-    var calculatedsum = sum(100) ;
+   
+    res.send("another route let's say for login and signup")
+
+}
+
+function handleSecondRequest(req,res){
+    // res.send('Hello WOrld!')
+    var counter = req.query.counter
+    console.log(req.query.counter)
+    console.log(req.query.counter2)
+    console.log(req.query.counter3)
+    var calculatedsum = sum(counter) ;
     console.log(calculatedsum);
     var answer = "the sum is " + calculatedsum;
     res.send(answer)
 
 }
+app.get('/handlesum',handleSecondRequest)
 app.get('/',handleFirstRequest)
+
+function createUser(req,res){
+    res.send("Hello World")
+}
+// POST
+app.post('/createUser',createUser)
 function started(){
     console.log(`Example app listening on port ${port}`)
 }
 app.listen(port,started)
+
 function sum  (counter){
     let csum=0;
     for(var i=1;i<=counter;i++){
