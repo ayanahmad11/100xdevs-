0ea74934e23a09f6dd1eaf863a4e096bc3d3c7bc
ayanahmad11 <ayan2011032@akgec.ac.in>
2024-02-16 19:35:24 +0530

    Frontend starts

---
+++ week4/todo_frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+var todos ={
+  title :"Go to GYM",
+  description :"go to gumm11",
+  id:1 
+
+}
+// setInterval(()=>{       // it will not work because it is not the way or it is not a state variable
+//   todos.title = "abcd", 
+//   todos.description = "no";
+// },1000);
+
+
+
+function App() {
+  
+  const [todos,setTodo] =  useState({
+    title:"go eat something ",
+    description:"eat food",
+    id:1
+  });
+  setInterval(()=>{
+    setTodo({
+      title:"title is changed",
+      description:"description is changed",
+      id :2
+    })
+  },2000)
+ 
+
+  return (
+    <>
+     <h5>hi there</h5>
+      {todos.title}<br>
+      </br>
+      {todos.description}
+      <br></br>
+      {todos.id}
+      <Personname firstname={"shahalam"} lastname ={"ajaz"} ></Personname> 
+     {/* props are like arguments */}
+      
+      <Personname firstname={todos.title} lastname = {todos.description}></Personname>
+    </>
+  )
+} 
+
+function Personname(props){    // this is a component
+  return <div>      
+    {props.firstname} 
+    <br></br>
+    {props.lastname}
+  </div>
+}
+
+export default App
